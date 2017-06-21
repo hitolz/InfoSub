4e7c131be6883a35bcc7cb511b5f7874b5ad82e1
hhHypo <i@ihypo.net>
2017-06-21 23:20:20 +0800

    add user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import app_config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -18,5 +20,6 @@ def create_app(config_name):
     app.register_blueprint(view_blueprint)
 
     db.init_app(app)
+    login_manager.init_app(app)
     return app
 
