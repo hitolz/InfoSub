1ea81e83a1d05b83f3e108704b2a89209c9f005f
CoderHito <hito@o0w0o.cn>
2017-07-05 18:58:28 +0800

    validata form

--- app/information/view.py
+++ app/information/view.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from app.information import info_blueprint
+from flask_login import login_required
+
+from app.information.form.site_form import SiteForm
 
 
-@info_blueprint.route('/')
-def infohome():
-    return render_template('subsite.html')
+@info_blueprint.route('/site', methods=["POST", "GET"])
+@login_required
+def site_info():
+    form = SiteForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('sub_site.html', form=form)
