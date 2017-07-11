463bd7de460e0b0806e2a90201115d845040fef3
hhHypo <i@ihypo.net>
2017-07-11 19:45:00 +0800

    写了一堆js

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -2,3 +2,72 @@
  * Created by hypo on 2017/6/23.
  */
 
+$("#captcha").click(function () {
+    $(this).html("");
+    $.ajax({
+        url: '/captcha',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $("#captcha").html('<img src="/captcha/' + data.captcha_id +
+                '" width="100%" class="img-rounded">');
+            $("input[name='captcha_id']").val(data.captcha_id)
+        },
+        error: function () {
+            alert("ERROR")
+        }
+    });
+});
+$("#username").bind('input propertychange', function () {
+    var in_name = $(this);
+    var class_val = "form-group ";
+    var username = in_name.val();
+    $.ajax({
+        url: '/verification/username',
+        type: 'POST',
+        data: {username: username},
+        dataType: 'json',
+        success: function () {
+            in_name.parent().attr("class", class_val + "has-success");
+        },
+        error: function () {
+            in_name.parent().attr("class", class_val + "has-error");
+        }
+    });
+});
+$("#email").bind('input propertychange', function () {
+    var in_email = $(this);
+    var class_val = "form-group ";
+    var email = in_email.val();
+    $.ajax({
+        url: '/verification/email',
+        type: 'POST',
+        data: {email: email},
+        dataType: 'json',
+        success: function () {
+            in_email.parent().attr("class", class_val + "has-success");
+        },
+        error: function () {
+            in_email.parent().attr("class", class_val + "has-error");
+        }
+    });
+
+});
+$("#captcha_code").bind('input propertychange', function () {
+    var in_cp = $(this);
+    var class_val = "col-md-8 ";
+    var cp_code = in_cp.val();
+    var cp_id = $("#captcha_id").val();
+    $.ajax({
+        url: '/verification/captcha',
+        type: 'POST',
+        data: {captcha_code: cp_code, captcha_id: cp_id},
+        dataType: 'json',
+        success: function () {
+            in_cp.parent().attr("class", class_val + "has-success");
+        },
+        error: function () {
+            in_cp.parent().attr("class", class_val + "has-error");
+        }
+    });
+});
--- app/view/login.py
+++ app/view/login.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect, url_for, send_file, abort
+from flask import render_template, redirect, url_for, send_file, abort, jsonify, request
 from flask_login import login_user, login_required, logout_user, current_user
 
 from app.view import view_blueprint
 from app.user.form.login import LoginForm, RegisterForm
 from app.services.user import get_user_by_username_or_email, create_user, init_user_plan
+from app.services.user import validate_email, validate_username
 from app.util.captcha_tool import Captcha
 
 
@@ -36,8 +37,14 @@ def register():
     return render_template("register.html", form=form, captcha_id=captcha.captcha_id)
 
 
+@view_blueprint.route("/captcha")
+def get_captcha():
+    captcha = Captcha()
+    return jsonify(dict(captcha_id=captcha.captcha_id))
+
+
 @view_blueprint.route("/captcha/<captcha_id>")
-def get_captcha(captcha_id):
+def get_captcha_image(captcha_id):
     captcha = Captcha.get_by_captcha_id(captcha_id)
     if not captcha:
         abort(404)
@@ -49,4 +56,31 @@ def get_captcha(captcha_id):
 def logout():
     logout_user()
     return redirect(url_for("view.login"))
+
+
+@view_blueprint.route("/verification/email", methods=["POST"])
+def verify_email():
+    email = request.form.get("email")
+    if email and validate_email(email):
+        return jsonify(dict(message="success")), 200
+    return jsonify(dict(message="email is illegal")), 400
+
+
+@view_blueprint.route("/verification/username", methods=["POST"])
+def verify_username():
+    username = request.form.get("username")
+    if username and validate_username(username):
+        return jsonify(dict(message="success")), 200
+    return jsonify(dict(message="username is illegal")), 400
+
+
+@view_blueprint.route("/verification/captcha", methods=["POST"])
+def verify_captcha():
+    captcha_id = request.form.get("captcha_id")
+    captcha_code = request.form.get("captcha_code")
+    captcha = Captcha.get_by_captcha_id(captcha_id)
+    if captcha and captcha.validate(captcha_code):
+        return jsonify(dict(message="success")), 200
+    return jsonify(dict(message="wrong captcha")), 400
+
 
